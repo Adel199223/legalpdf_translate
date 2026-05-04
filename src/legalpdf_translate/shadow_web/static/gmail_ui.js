98ffaaad1dca950af08197977e4db34ba2ccb68b
Adel Belghali <adel.belghali@gmail.com>
2026-05-04 23:34:38 +0100

    Extract Gmail control state renderers

--- src/legalpdf_translate/shadow_web/static/gmail_ui.js
+++ src/legalpdf_translate/shadow_web/static/gmail_ui.js
@@ -1,6 +1,36 @@
 import { clearNode, createTextElement, setNodeTitle, setText } from "./safe_rendering.js";
 import { appendResultGridItem, createResultHeader } from "./result_card_ui.js";
 
+export function renderGmailDrawerDatasetDefaultsInto(body) {
+  if (!body) {
+    return undefined;
+  }
+
+  body.dataset.gmailReviewDrawer = "closed";
+  body.dataset.gmailPreviewDrawer = "closed";
+  body.dataset.gmailSessionDrawer = "closed";
+  body.dataset.gmailBatchFinalizeDrawer = "closed";
+  return body;
+}
+
+export function renderGmailDetailsOpenInto(details, { open = false } = {}) {
+  if (!details) {
+    return undefined;
+  }
+
+  details.open = Boolean(open);
+  return details;
+}
+
+export function renderGmailInputValueInto(input, value = "") {
+  if (!input) {
+    return undefined;
+  }
+
+  input.value = String(value ?? "");
+  return input;
+}
+
 export function renderGmailDrawerChromeInto(nodes = {}, drawer = {}) {
   const { backdrop, body } = nodes || {};
   if (!backdrop) {
